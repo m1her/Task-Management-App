83fee2c79e35215a5f288b876b2cde0987bb6003
m1her <maher.ple2@gmail.com>
2024-05-27 21:32:51 +0300

    start to add to firebase

--- src/Firebase/firebase-config/index.ts
+++ src/Firebase/firebase-config/index.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,9 +15,10 @@ const firebaseConfig = {
   storageBucket: "task-management-app-13316.appspot.com",
   messagingSenderId: "867657670664",
   appId: "1:867657670664:web:e00b1c89beef3712b1b1bd",
-  measurementId: "G-SVK9T93L1M"
+  measurementId: "G-SVK9T93L1M",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth =  getAuth(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -2,8 +2,13 @@ import React, { useCallback, useState } from "react";
 import { Task } from "../../Components/Task";
 import "./style.css";
 import { Reorder } from "framer-motion";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../Firebase/firebase-config";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 
 export const Dashboard = () => {
+  const [user] = useAuthState(auth);
+
   const [toDoTasks, setToDoTasks] = useState([
     { id: 1 },
     { id: 2 },
@@ -33,13 +38,28 @@ export const Dashboard = () => {
     setDragToggler(false);
   }, []);
 
+  const addTaskHandler = () => {
+    if (user) {
+      addDoc(collection(db, user.email || ""), {
+        id: Math.random(),
+        title: "task 2",
+        description: "asdasdasda asdasd asda sdas d",
+        due: "10:30am | 25-3-2024",
+        status: "to-do",
+      });
+    }
+  };
+
   return (
     <div>
       <nav className="dash-title">Task Manager</nav>
       <div className="w-full flex justify-center items-center">
-        {/* <div className="text-lg font-semibold bg-[rgb(59,130,246)] text-white flex justify-center items-center rounded py-1 px-12 cursor-pointer hover:bg-[rgb(70,139,251)] shadow-[0_0_7px_2px_rgba(59,130,246,0.3)] hover:shadow-[0_0_7px_2px_rgba(70,139,251,0.3)] transition-all ">
+        <div
+          className="text-lg font-semibold bg-[rgb(59,130,246)] text-white flex justify-center items-center rounded py-1 px-12 cursor-pointer hover:bg-[rgb(70,139,251)] shadow-[0_0_7px_2px_rgba(59,130,246,0.3)] hover:shadow-[0_0_7px_2px_rgba(70,139,251,0.3)] transition-all "
+          onClick={addTaskHandler}
+        >
           Add Task
-        </div> */}
+        </div>
       </div>
       <div className="dash-grid">
         <div className="dash-piller-container">

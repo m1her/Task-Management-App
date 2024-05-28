6339f8a86612066ee5d02c263b0c7e7fd4a16ada
m1her <maher.ple2@gmail.com>
2024-05-28 21:18:34 +0300

    tasks grap and drop fully working

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,42 +1,85 @@
-import React, { useCallback, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Task } from "../../Components/Task";
 import "./style.css";
 import { Reorder } from "framer-motion";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../Firebase/firebase-config";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
 export const Dashboard = () => {
   const [user] = useAuthState(auth);
+  const [statusFlag, setstatusFlag] = useState("");
+  const [tasks, setTasks] = useState<any>();
+  const [toDo, setToDo] = useState<any>([]);
+  const [inProgress, setInProgress] = useState<any>([]);
+  const [done, setDone] = useState<any>([]);
 
-  const [toDoTasks, setToDoTasks] = useState([
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-  ]);
-  const [inProgressTasks, setInProgressTasks] = useState([
-    { id: 6 },
-    { id: 7 },
-  ]);
-  const [doneTasks, setDoneTasks] = useState([{ id: 8 }]);
   const [selectedTask, setSelectedTask] = useState<any>({});
   const [dragToggler, setDragToggler] = useState(false);
 
-  const onMouseDown = useCallback(
-    (id: number) => {
-      const selected = toDoTasks.find((task) => task.id === id);
-      setSelectedTask(selected);
-      setDragToggler(true);
-      console.log(selected);
-    },
-    [toDoTasks]
-  );
+  const onMouseDown = (id: number) => {
+    const selected = tasks.find((task: { id: number }) => task.id === id);
+    setSelectedTask(selected);
+    setDragToggler(true);
+  };
 
-  const onMouseUp = useCallback(() => {
+  const onMouseUp = async () => {
+    if (selectedTask.status !== statusFlag) {
+      if (statusFlag === "to-do") {
+        setToDo((prev: any) => [...prev, { ...selectedTask, status: "to-do" }]);
+      }
+      if (statusFlag === "in-progress") {
+        setInProgress((prev: any) => [
+          ...prev,
+          { ...selectedTask, status: "in-progress" },
+        ]);
+      }
+      if (statusFlag === "done") {
+        setDone((prev: any) => [...prev, { ...selectedTask, status: "done" }]);
+        console.log(done);
+      }
+      if (selectedTask?.status === "to-do") {
+        setToDo((prev: any) =>
+          prev.filter((task: any) => task.id !== selectedTask.id)
+        );
+      }
+      if (selectedTask?.status === "in-progress") {
+        setInProgress((prev: any) =>
+          prev.filter((task: any) => task.id !== selectedTask.id)
+        );
+      }
+      if (selectedTask?.status === "done") {
+        setDone((prev: any) =>
+          prev.filter((task: any) => task.id !== selectedTask.id)
+        );
+      }
+    }
+
+    if (user) {
+      const q = query(
+        collection(db, user?.email || ""),
+        where("id", "==", selectedTask.id)
+      );
+
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot) {
+        const docRef = doc(db, user?.email || "", querySnapshot.docs[0].id);
+        // await updateDoc(docRef, {
+        //   status: statusFlag,
+        // });
+      }
+    }
     setDragToggler(false);
-  }, []);
+  };
 
   const addTaskHandler = () => {
     if (user) {
@@ -49,6 +92,36 @@ export const Dashboard = () => {
       });
     }
   };
+  useEffect(() => {
+    if (user) {
+      const fetchDocuments = async () => {
+        const querySnapshot = await getDocs(collection(db, user?.email || ""));
+        const docs = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setTasks(docs);
+      };
+
+      fetchDocuments();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (tasks) {
+      setToDo(
+        tasks.filter((task: { status: string }) => task.status === "to-do")
+      );
+      setInProgress(
+        tasks.filter(
+          (task: { status: string }) => task.status === "in-progress"
+        )
+      );
+      setDone(
+        tasks.filter((task: { status: string }) => task.status === "done")
+      );
+    }
+  }, [tasks]);
 
   return (
     <div>
@@ -68,29 +141,34 @@ export const Dashboard = () => {
               className={`dash-drag-toggeler
               ${!dragToggler ? "hidden" : "inline"}
               `}
+              onMouseOver={() => setstatusFlag("to-do")}
             ></div>
             <div className="dash-piller-title text-[#EF4444]">To Do</div>
 
-            <Reorder.Group
-              axis="y"
-              values={toDoTasks}
-              onReorder={setToDoTasks}
-              className="dash-reorder"
-            >
-              {toDoTasks.map((task) => (
-                <Reorder.Item
-                  key={task.id}
-                  value={task}
-                  drag
-                  dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                  dragElastic={1}
-                  onDragEnd={onMouseUp}
-                  onDragStart={() => onMouseDown(task.id)}
-                >
-                  <Task key={task.id} id={task.id} />
-                </Reorder.Item>
-              ))}
-            </Reorder.Group>
+            {toDo && (
+              <Reorder.Group
+                axis="y"
+                values={toDo}
+                onReorder={setToDo}
+                className="dash-reorder"
+              >
+                {toDo
+                  .filter((task: { status: string }) => task.status === "to-do")
+                  .map((task: { id: number }) => (
+                    <Reorder.Item
+                      key={task.id}
+                      value={task}
+                      drag
+                      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                      dragElastic={1}
+                      onDragEnd={onMouseUp}
+                      onDragStart={() => onMouseDown(task.id)}
+                    >
+                      <Task key={task.id} id={task.id} />
+                    </Reorder.Item>
+                  ))}
+              </Reorder.Group>
+            )}
           </div>
         </div>
         <div className="dash-piller-container">
@@ -99,28 +177,35 @@ export const Dashboard = () => {
               className={`dash-drag-toggeler
                ${!dragToggler ? "hidden" : "inline"}
                `}
+              onMouseOver={() => setstatusFlag("in-progress")}
             ></div>
             <div className="dash-piller-title text-[#FBBF24]">In Progress</div>
-            <Reorder.Group
-              axis="y"
-              values={inProgressTasks}
-              onReorder={setInProgressTasks}
-              className="dash-reorder"
-            >
-              {inProgressTasks.map((task) => (
-                <Reorder.Item
-                  key={task.id}
-                  value={task}
-                  drag
-                  dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                  dragElastic={1}
-                  onDragEnd={onMouseUp}
-                  onDragStart={() => onMouseDown(task.id)}
-                >
-                  <Task key={task.id} id={task.id} />
-                </Reorder.Item>
-              ))}
-            </Reorder.Group>
+            {inProgress && (
+              <Reorder.Group
+                axis="y"
+                values={inProgress}
+                onReorder={setInProgress}
+                className="dash-reorder"
+              >
+                {inProgress
+                  .filter(
+                    (task: { status: string }) => task.status === "in-progress"
+                  )
+                  .map((task: { id: number }) => (
+                    <Reorder.Item
+                      key={task.id}
+                      value={task}
+                      drag
+                      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                      dragElastic={1}
+                      onDragEnd={onMouseUp}
+                      onDragStart={() => onMouseDown(task.id)}
+                    >
+                      <Task key={task.id} id={task.id} />
+                    </Reorder.Item>
+                  ))}
+              </Reorder.Group>
+            )}
           </div>
         </div>
         <div className="dash-piller-container">
@@ -129,27 +214,31 @@ export const Dashboard = () => {
               className={`dash-drag-toggeler ${
                 !dragToggler ? "hidden" : "inline"
               }`}
+              onMouseOver={() => setstatusFlag("done")}
             ></div>
             <div className="dash-piller-title text-[#34D399]">Done</div>
+
             <Reorder.Group
               axis="y"
-              values={doneTasks}
-              onReorder={setDoneTasks}
+              values={done}
+              onReorder={setDone}
               className="dash-reorder"
             >
-              {doneTasks.map((task) => (
-                <Reorder.Item
-                  key={task.id}
-                  value={task}
-                  drag
-                  dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                  dragElastic={1}
-                  onDragEnd={onMouseUp}
-                  onDragStart={() => onMouseDown(task.id)}
-                >
-                  <Task key={task.id} id={task.id} />
-                </Reorder.Item>
-              ))}
+              {done
+                .filter((task: { status: string }) => task.status === "done")
+                .map((task: { id: number }) => (
+                  <Reorder.Item
+                    key={task.id}
+                    value={task}
+                    drag
+                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                    dragElastic={1}
+                    onDragEnd={onMouseUp}
+                    onDragStart={() => onMouseDown(task.id)}
+                  >
+                    <Task key={task.id} id={task.id} />
+                  </Reorder.Item>
+                ))}
             </Reorder.Group>
           </div>
         </div>
